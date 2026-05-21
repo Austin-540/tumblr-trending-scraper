a72b2f6b139ec9dff2129aef3418b20aa8622f2e
Austin <105258719+Austin-540@users.noreply.github.com>
2026-05-21 19:13:40 +1200

    Initial commit

---
+++ grab_the_data.R
@@ -0,0 +1,29 @@
+library(rvest)
+library(tidyverse)
+library(jsonlite)
+
+read_html("https://www.tumblr.com/robots.txt") %>%
+  html_text2() %>%
+  str_detect("explore|trending") %>%
+  if (.) {
+    stop("Hold up! The robots.txt file now mentions the trending page... check if this is still allowed")
+  }
+
+trending_url <- "https://www.tumblr.com/explore/trending"
+
+page <- read_html(trending_url)
+
+script_with_trending <- page %>% html_element("script#___INITIAL_STATE___") %>%
+  html_text2()
+
+tumblr_function_data <- fromJSON(script_with_trending)
+
+trending_data <- tumblr_function_data$Explore$trendingTagsTimeline$elements
+
+trending_tags <- trending_data$topicTitle
+
+output <- paste0(trending_tags, collapse="}")
+
+output_with_date <- paste0(now(), "^", output)
+
+write(output_with_date, file="trending_data.txt", append = TRUE)
